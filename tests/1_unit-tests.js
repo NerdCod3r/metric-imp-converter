4a40c6655245ee27570b251658c29396aa4a56ff
root <root@A>
2025-03-29 09:38:57 +0200

    chore:finished-16-unit-tests.-They-like-they-work.

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -65,5 +65,90 @@ suite('Unit Tests', function(){
         done();
     });
 
+    // test #9
+    test("#correctlyReadInputUnitAndReturnCorrectReturnUnit", function(done){
+            let inputUnits = ["gal", "L", "mi", "km", "lbs", "kg"];
+            let outputUnits =["L", "gal", "km", "mi", "kg", "lbs"];
+
+            inputUnits.forEach((Unit, Index)=>{
+                assert.equal(convertHandler.getReturnUnit(Unit), outputUnits[Index])
+            });
+            done();
+    });
+
+    // test #10
+    test("#forEachValidInputUnit_ReturnASpellOutForTheUnit", function(done){
+        let inputUnits = ["gal", "L", "mi", "km", "lbs", "kg"];
+        let spelledOutInputUnits =  ["gallons", "liters", "miles", "kilometers", "pounds", "kilograms" ];
+
+        inputUnits.forEach((InputUnit, index)=>{
+            assert.equal(convertHandler.spellOutUnit(InputUnit), spelledOutInputUnits[index]);
+        });
+
+        done();
+    });
+
+    // test #11
+    test("#correctlyConvert_gal_To_L", function(done){
+        let input = "gal";
+        let inputNumber = convertHandler.getNum(input);
+        let inputUnit = convertHandler.getUnit(input);
+        const convertResult = convertHandler.convert(inputNumber, inputUnit);
+        const expected = 3.78541;
+
+        assert.approximately(convertResult.returnNum, expected, 0.1);
+        done();
+    });
+
+    // test #12
+    test("#correctly_convert_L_to_gal", function(done){
+        const expectedVal = 1;
+        const converted_object = convertHandler.convert(3.78541, "L");
+        assert.approximately(converted_object.returnNum, expectedVal, 0.1);
+
+        done();
+    });
+
+    // test #13
+    test("#correctly_convert_mi_To_km", function(done){
+        const expected_km = 1.60934;
+        const convert_obj = convertHandler.convert(1, "mi");
+
+        assert.approximately(convert_obj.returnNum, expected_km, 0.1);
+        done();
+    });
+
+    // test #14
+    test("#correctly_convert_km_To_mi", function(done){
+        const expectedMi = 1;
+        const initialKm = 1.60934;
+        const return_converted_object = convertHandler.convert(initialKm, "km");
+
+        assert.approximately(return_converted_object.returnNum, expectedMi, 0.1);
+
+        done();
+    });
+
+    // test #15
+    test("#correctly_convert_lbs_to_kg", function(done){
+        const expectedKg = 0.453592;
+        const initLbs = 1;
+        const return_object = convertHandler.convert(initLbs, "lbs");
+
+        assert.approximately(return_object.returnNum, expectedKg, 0.1);
+
+        done();
+    });
+
+    // test #16
+    test("#correctly_convert_kgs_to_lbs", function(done){
+        const expectedLbs = 1;
+        const initKgs = 0.453592;
+        const convert_return_object = convertHandler.convert(initKgs, "kg");
+
+        assert.approximately(convert_return_object.returnNum, expectedLbs, 0.1);
+
+        done();
+    });
 
 });
